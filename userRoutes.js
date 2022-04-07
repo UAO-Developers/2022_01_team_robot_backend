70864db750c1a59c93b37544adf4bf5f75e9d1ce
Sebastian Garcia <joan_seb.garcia@uao.edu.co>
2022-04-07 11:05:18 -0500

    init commit

---
+++ userRoutes.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// create App function
+    module.exports = function(app) {
+        var userList = require('./modelController');
+
+// todoList Routes
+
+// get and post request for /todos endpoints
+        app
+        .route("/user")
+        .get(userList.listAllUsers)
+        .post(userList.createNewUser);
+
+// put and delete request for /todos endpoints
+        app
+        .route("/user/:id")
+        .put(userList.updateUser)
+        .delete(userList.deleteUser);
+
+        app
+        .route("/login")
+        .post(userList.login)
+
+        app
+        .route("/match")
+        .post(userList.createNewMatch)
+        .get(userList.listAllMatcher)
+    };
